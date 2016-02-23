057cc34390632b67fdc13e927883b545c0290903
Alex Lin <alexander.s.lin@nasa.gov>
2016-02-23 14:38:49 -0600

    Split test sims and fun sims into separate directories.
    Moved more sims into the test directory.
    
    refs #191

--- trick_sims/SIM_test_inherit/RUN_test/input.py
+++
@@ -1,7 +0,0 @@
-
-def main():
-    trick.stop(300.0)
-
-if __name__ == "__main__":
-    main()
-
--- trick_sims/SIM_test_sched/RUN_test/input.py
+++
@@ -1,7 +0,0 @@
-
-#execfile("Modified_data/data_record.dr")
-#execfile("Modified_data/realtime.py")
-
-my_integ_loop.getIntegrator(trick.Runge_Kutta_4, 2)
-trick.sim_services.exec_set_terminate_time(300.0)
-#trick.echo_jobs_on();
