a006fd0c8136038ebc019eed2be24f79164a71a8
Alex Lin <alexander.s.lin@nasa.gov>
2016-02-23 13:48:59 -0600

    plit test sims and fun sims into separate directories
    Moved SIM_events, test_dp, test_dr, and abstract.
    
    refs #191

--- trick_sims/SIM_test_dp/Modified_data/data_record.py
+++
@@ -1,5 +0,0 @@
-
-execfile("Modified_data/group1.dr")
-execfile("Modified_data/group2.dr")
-execfile("Modified_data/group3.dr")
-execfile("Modified_data/group4.dr")
--- trick_sims/SIM_test_dp/RUN_test/input.py
+++
@@ -1,5 +0,0 @@
-
-execfile("Modified_data/data_record.py")
-
-
-trick.sim_services.exec_set_terminate_time(10.0)
